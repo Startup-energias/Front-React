375a5dda0ddbe0d8dd02e977bb2ab3c2dc7aa10a
chrisdev12 <christianm.bravop@gmail.com>
2021-01-30 17:22:30 -0500

    Update phaseSection styles

--- src/components/home/PhraseSection.jsx
+++ src/components/home/PhraseSection.jsx
@@ -1,26 +1,22 @@
 import people from '../../assets/images/home/gente.png';
+import './scss/_phraseSection.scss';
 
-function PhraseSection(){
-    return(
-        <section className="phrase section">
-            <div className="columns is-gapless">
-                <div className="column is-6 is-flex is-justify-content-center is-align-items-flex-end is-flex-direction-column">
-                    <div className="block m-0 is-flex is-justify-content-flex-end is-align-items-flex-start is-flex-direction-column">
-                        <p className="is-size-4 title m-0">Are you passionate about renewable energy? </p>
-                        <p className="is-size-4">We too! Join Inoverte community.</p>
-                    </div>
-                    <div className="columns">
-                        <div className="column is-offset-4 pl-3 ">
-                            <p className="is-size-6">We find human talent and funds to support innovative renewable energy projects from universities that provide electricity and water solutions.</p>
-                        </div>
-                    </div>
-                </div>
-                <div className="column is-6 is-flex is-justify-content-flex-start is-align-items-center is-flex-direction-row">
-                    <img src={people} alt="" />
-                </div>
-            </div>
-        </section>
-    );
+function PhraseSection() {
+  return (
+    <section className="phraseSection is-flex is-flex-wrap-wrap	is-justify-content-center is-align-items-center">
+      <div className="py-3 mr-2">
+        <div className="phraseSection__title mb-5">
+          <p className="title mb-1">Are you passionate about renewable energy?</p>
+          <p>We too! Join Inoverte community.</p>
+        </div>
+        <p>
+          We find human talent and funds to support innovative renewable energy projects from
+          universities that provide electricity and water solutions.
+        </p>
+      </div>
+      <img className="mt-4" src={people} alt="people collaborating to get clean energies" />
+    </section>
+  );
 }
 
 export default PhraseSection;
