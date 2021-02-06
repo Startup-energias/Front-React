92ed5f30c4e1997497d4a79ccae54facd7c6723c
Nicolas Abondano <nf.abondano@uniandes.edu.co>
2021-02-05 23:43:16 -0500

    About corrections

--- src/components/about/AboutComponent.jsx
+++ src/components/about/AboutComponent.jsx
@@ -1,12 +1,11 @@
-
 import './scss/_aboutComponent.scss';
 
 function AboutComponent({ right, title, color, text, image }) {
   return (
-    <div className={`item ${right && 'about__right'} ${!right && 'about__left'} is-flex mt-6`}>
+    <div className={`item ${right && 'about__right'} ${!right && 'about__left'} is-flex`}>
       <div className={`item__content is-flex is-flex-direction-column ${image && 'item__reduced'}`}>
         <p
-          className="item__title"
+          className="item__title is-flex-grow-0"
           style={{
             color: `${color}`,
           }}
@@ -14,21 +13,17 @@ function AboutComponent({ right, title, color, text, image }) {
           OUR
         </p>
         <div
-          className=""
+          className="is-flex-grow-1"
           style={{
             backgroundColor: `${color}`,
           }}
         >
-            <p className="item__title has-text-white">
-                {title}
-            </p>
-            <p className="item__subtext is-size-6 has-text-white">
-                {text}
-            </p>
+          <p className="item__title has-text-white">{title}</p>
+          <p className="item__subtext is-size-6 has-text-white">{text}</p>
         </div>
       </div>
-      
-      {image && <img className="item__image" src={image} alt={title+"__image"} />}
+
+      {image && <img className="item__image" src={image} alt={title + '__image'} />}
     </div>
   );
 }
--- src/views/About.jsx
+++ src/views/About.jsx
@@ -5,9 +5,6 @@ import AboutComponent from '../components/about/AboutComponent';
 function About() {
   return (
     <div className="about is-flex is-flex-direction-column is-aling-items-center mb-5">
-      <div className="about__banner">
-        <img src={aboutInfo.banner} alt="banner img" />
-      </div>
       {aboutInfo.items.map((item) => (
         <AboutComponent {...item} key={item.title + 'component'} />
       ))}
