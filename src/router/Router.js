20727603067757ecb4d01d98b148ca3cb34850a1
chrisdev12 <christianm.bravop@gmail.com>
2021-02-06 17:09:44 -0500

    Created privateRoute HOC and tested it with profile preview

--- src/router/Router.js
+++ src/router/Router.js
@@ -1,13 +1,14 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 //Lazy loading optimization
 import { lazy, Suspense } from 'react';
-
 const Index = lazy(() => import('../views/Index'));
 const HelpNow = lazy(() => import('../views/HelpNow'));
 const Project = lazy(() => import('../views/Project'));
+import Profile from '../views/Profile';
 import Navbar from '../components/shared/Navbar';
 import Footer from '../components/shared/Footer';
 import { indexItems } from '../helpers/constants/navbarItems';
+import PrivateRoute from './PrivateRoute';
 
 const indexRouter = () => (
   <BrowserRouter>
@@ -16,6 +17,7 @@ const indexRouter = () => (
       <Switch>
         <Route exact path="/help" component={HelpNow} />
         <Route exact path="/projects" component={Project} />
+        <PrivateRoute exact path="/profile" component={Profile} />
         <Route path="/" component={Index} />
       </Switch>
     </Suspense>
