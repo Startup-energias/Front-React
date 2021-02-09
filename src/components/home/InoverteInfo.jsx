0d3f5c0c3cb0fa4ed4abfa23070f5de0b464557f
Nicolas Abondano <nf.abondano@uniandes.edu.co>
2021-02-09 17:43:23 -0500

    More responsive

--- src/components/home/InoverteInfo.jsx
+++ src/components/home/InoverteInfo.jsx
@@ -1,11 +1,12 @@
 import { sections } from '../../helpers/constants/inoverteInfo';
 import InfoCard from '../shared/InfoCard';
+import './scss/_inoverteInfo.scss';
 
 function InoverteInfo() {
   return (
-    <section className="section info pt-0 mt-6" id="About us">
+    <section className="section info mt-6" id="About us">
       <h1 className="has-text-centered is-1 title has-text-dark">Features</h1>
-      <div className="is-flex is-justify-content-center is-flex-wrap-wrap mt-6">
+      <div className="infoCards is-flex is-flex-wrap-wrap is-justify-content-space-evenly">
         {sections.map((section, id) => (
           <InfoCard section={section} id={0} key={'section-' + id} />
         ))}
