11359237e68bfb3e58672084ad8a2d3bd64e68af
Nicolas Abondano <nf.abondano@uniandes.edu.co>
2021-02-10 19:40:24 -0500

    About components adjustments

--- src/components/about/DoubleComponent.jsx
+++ src/components/about/DoubleComponent.jsx
@@ -3,16 +3,17 @@ import './scss/_doubleComponent.scss';
 function DoubleComponent({ title, color, text, image }) {
   return (
     <div className={`double is-flex`}>
+      {image && <img className="double__image mb-6" src={image} alt={title + '__image'} />}
       <div
         className={`double__content is-flex is-flex-direction-column ${image && 'double__reduced'}`}
       >
         <p
-          className="double__title is-flex-grow-0"
+          className="double__title is-flex-grow-0 is-uppercase"
           style={{
             color: `${color}`,
           }}
         >
-          OUR
+          Our
         </p>
         <div
           className="is-flex-grow-1"
@@ -20,12 +21,10 @@ function DoubleComponent({ title, color, text, image }) {
             backgroundColor: `${color}`,
           }}
         >
-          <p className="double__title has-text-white">{title}</p>
-          <p className="double__subtext is-size-6 has-text-white">{text}</p>
+          <p className="double__title has-text-white is-uppercase">{title}</p>
+          <p className="double__subtext is-size-4 has-text-white">{text}</p>
         </div>
       </div>
-
-      {image && <img className="double__image" src={image} alt={title + '__image'} />}
     </div>
   );
 }
--- src/helpers/constants/aboutInfo.js
+++ src/helpers/constants/aboutInfo.js
@@ -32,27 +32,27 @@ export const aboutInfo = {
     ],
     items: [{
         right: false,
-        title: 'MISION',
+        title: 'Mision',
         color: "#247dff",
         text: "To drive renewable innovation by connecting entrepreneurs, off-grid communities, and investors worldwide.",
         image: images__about + 'mision.jpg'
     }, {
         right: true,
-        title: 'VISION',
+        title: 'Vision',
         color: "#23cee0",
-        text: "Invoverte is a Colombian initiative supported by START Global that is taking off in the Swiss entrepreneurship ecosystem. We are inspired by the need to connect and create a global community that promotes sustainability and innovation. Using the web environment, we help to make visible innovative solutions to problems related to basic resources, promoting the creation of teams with human talents capable of positively impacting the new generations through sustainable ideas. We will be the company that will lead the energy transformation towards a society where basic resources such as electricity and water are affordable for all using renewable technologies. Our goal is to supply water and / or energy to 3 million people by 2025. In this way, we visualize all the projects in our Inverte platform financed, developed and implemented. By 2022, Inverte will be the most referenced platform in the energy sector, with 1 million active users around the world. ",
+        text: "We envision a world where sustainable technologies drive our society. We introduce a digital platform to Latin America that fits this vision. At Inverte, we promote sustainability, as we connect renewable innovation and financing to benefit off-grid communities.",
         image: images__about + 'vision.jpg'
     }, {
         right: false,
-        title: 'PATH',
+        title: 'Path',
         color: "#1a2340",
-        text: "Impulsar el desarrollo tecnológico sostenible con el objetivo de brindar acceso a los recursos básicos de electricidad y agua potable. Conectamos proyectos sostenibles con financiación y talento humano para que puedan ser implementados en las comunidades que lo necesitan a través de la visibilización y difusión que fomenta nuestra plataforma",
+        text: "Invoverte is a Colombian initiative supported by START Global that is taking off in the Swiss entrepreneurship ecosystem.",
     },
     {
         right: true,
-        title: 'DRIVERS',
+        title: 'Values',
         color: "#a3d9ea",
-        text: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesentconsequat.",
+        text: "We are governed by the Sustainable Development Goals SDGs from the United Nations UN 6. Clean water and 7. Affordable and clean energy.",
         image: images__about + 'drivers.png'
     },
     ]
