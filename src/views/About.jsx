0bbd10ec59e051369a34923d843230618a7c34c9
Nicolas Abondano <nf.abondano@uniandes.edu.co>
2021-02-11 18:35:47 -0500

    beginning with new changes

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -1,18 +1,23 @@
+/* eslint-disable no-unused-vars */
 import './scss/_about.scss';
+import { useState, useEffect } from 'react';
 import { aboutInfo } from '../helpers/constants/aboutInfo';
 import AboutComponent from '../components/about/AboutComponent';
 import DoubleComponent from '../components/about/DoubleComponent';
-//import TeamComponent from '../components/about/TeamComponent';
+import TeamComponent from '../components/about/TeamComponent';
 
 function About() {
   return (
     <div className="about is-flex is-flex-direction-column is-aling-items-center mb-5">
+      <div className="about__selector">
+        <div className="has-background-primary"></div>
+      </div>
       {aboutInfo.items.map((item, i) => (
         <div key={'component_item_' + i}>
           {i === 3 ? <DoubleComponent {...item} /> : <AboutComponent {...item} />}
         </div>
       ))}
-      {/*<TeamComponent />*/}
+      <TeamComponent />
     </div>
   );
 }
