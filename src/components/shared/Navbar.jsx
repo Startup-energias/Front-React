ad8621eb20f9aeabb0a6f5476bc71f38b5eabb16
Nicolas Abondano <nf.abondano@uniandes.edu.co>
2021-02-11 21:31:40 -0500

    navbar centered

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -30,21 +30,18 @@ function Navbar({ items }) {
         </button>
       </div>
       <div id="navbarBasicExample" className={`${navToggle && 'is-active'} navbar-menu`}>
-        <div className="navbar-end pr-4">
-          <a href={`/#${items[0]}`} className="navbar-item mx-6 has-text-dark">
-            {items[0]}
-          </a>
-          <a href={`/about`} className="navbar-item mx-6 has-text-dark">
-            {items[1]}
-          </a>
-          <a href={`/#${items[2]}`} className="navbar-item mx-6 has-text-dark">
-            {items[2]}
-          </a>
-          {/*items.map((e) => (
+        <a href={`/#${items[0]}`} className="navbar-item mx-6 has-text-dark">
+          {items[0]}
+        </a>
+        <a href={`/about`} className="navbar-item mx-6 has-text-dark">
+          {items[1]}
+        </a>
+        {/*items.map((e) => (
             <a key={e} href={`/${e}`} className="navbar-item mx-6 has-text-dark">
               {e}
             </a>
           ))*/}
+        <div className="navbar-end pr-4">
           <div className="navbar-divider" />
           <div className="navbar-item navbar__registration">
             <div className="buttons is-justify-content-center">
