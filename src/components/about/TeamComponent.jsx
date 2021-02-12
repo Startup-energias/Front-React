d89eaef67c2674d69726a1090f608e9408f0cdb2
Nicolas Abondano <nf.abondano@uniandes.edu.co>
2021-02-11 20:55:51 -0500

    About component finished

--- src/components/about/TeamComponent.jsx
+++ src/components/about/TeamComponent.jsx
@@ -1,12 +1,51 @@
+/* eslint-disable no-unused-vars */
 import './scss/_aboutTeam.scss';
-import { aboutInfo } from '../../helpers/constants/aboutInfo';
-
-function TeamComponent() {
+function TeamComponent({ title, color, team, advisors }) {
   return (
-    <div className="team  is-flex is-flex-direction-column">
-      <div className="team__image">
-        <img src={aboutInfo.team} alt="team img" />
-        <h1 className="team__title title is-uppercase has-text-white">Team</h1>
+    <div className={'team'}>
+      <div className={'team__text is-flex is-flex-direction-column'}>
+        <p className={'team__title is-flex-grow-0 is-uppercase has-text-' + color}>Our</p>
+        <div className={'is-flex-grow-1 has-background-' + color}>
+          <p className="team__title has-text-white is-uppercase">{title}</p>
+        </div>
+      </div>
+      <div className="team__members is-flex is-flex-wrap-wrap is-justify-content-center is-multiline mx-3">
+        {team.map((member, i) => (
+          <div
+            className="team__member is-flex is-flex-direction-column has-background-dark"
+            key={'member-' + i}
+          >
+            <img
+              className="team__image"
+              src={
+                member.image ? member.image : process.env.REACT_APP_IMAGES_SRC + 'team/default.png'
+              }
+              alt={member.name + '-image'}
+            />
+            <h2 className="has-text-centered has-text-primary is-size-2">{member.name}</h2>
+            <h3 className="has-text-centered has-text-primary is-size-4">{member.role}</h3>
+            <h4 className="has-text-centered has-text-primary is-size-6">{member.email}</h4>
+          </div>
+        ))}
+      </div>
+
+      <div className="team__members is-flex is-flex-wrap-wrap is-justify-content-center is-multiline mx-3">
+        {advisors.map((member, i) => (
+          <div
+            className="team__member is-flex is-flex-direction-column has-background-info"
+            key={'member-' + i}
+          >
+            <img
+              className="team__image"
+              src={
+                member.image ? member.image : process.env.REACT_APP_IMAGES_SRC + 'team/default.png'
+              }
+              alt={member.name + '-image'}
+            />
+            <h2 className="has-text-centered has-text-white is-size-2">{member.name}</h2>
+            <h3 className="has-text-centered has-text-white is-size-4">{member.role}</h3>
+          </div>
+        ))}
       </div>
     </div>
   );
--- src/views/About.jsx
+++ src/views/About.jsx
@@ -7,17 +7,34 @@ import DoubleComponent from '../components/about/DoubleComponent';
 import TeamComponent from '../components/about/TeamComponent';
 
 function About() {
+  let [selectedArr, setSelected] = useState([true, false, false, false, false]);
+
+  function changeSelected(index) {
+    let temp = [false, false, false, false, false];
+    temp[index] = true;
+    setSelected(temp);
+  }
   return (
     <div className="about is-flex is-flex-direction-column is-aling-items-center mb-5">
-      <div className="about__selector">
-        <div className="has-background-primary"></div>
+      <div className="about__selector buttons has-addons is-centered">
+        {aboutInfo.items.map((item, i) => (
+          <button
+            className={
+              'about__option button is-uppercase has-text-centered has-text-white has-background-' +
+              item.color
+            }
+            onClick={() => changeSelected(i)}
+            key={'selector_' + i}
+          >
+            {item.title}
+          </button>
+        ))}
       </div>
-      {aboutInfo.items.map((item, i) => (
-        <div key={'component_item_' + i}>
-          {i === 3 ? <DoubleComponent {...item} /> : <AboutComponent {...item} />}
-        </div>
-      ))}
-      <TeamComponent />
+      {selectedArr[0] && <AboutComponent {...aboutInfo.items[0]} />}
+      {selectedArr[1] && <AboutComponent {...aboutInfo.items[1]} />}
+      {selectedArr[2] && <AboutComponent {...aboutInfo.items[2]} />}
+      {selectedArr[3] && <DoubleComponent {...aboutInfo.items[3]} />}
+      {selectedArr[4] && <TeamComponent {...aboutInfo.items[4]} />}
     </div>
   );
 }
