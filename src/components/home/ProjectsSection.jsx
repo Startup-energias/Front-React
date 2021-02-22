025cf151b1ba6b68c1b7ab9c5c07d893e0b96347
Nicolas Abondano <nf.abondano@uniandes.edu.co>
2021-02-21 23:01:03 -0500

    Small corrections

--- src/components/home/ProjectsSection.jsx
+++ src/components/home/ProjectsSection.jsx
@@ -11,13 +11,14 @@ function ProjectsSection() {
   const projectsUrl = process.env.REACT_APP_INOVERTE_API + 'projects';
   //Slice
   const limit = 9;
+  const small = 823;
   const group = 3;
   const smallGroup = 1;
   let [isBig, setBig] = useState(false);
   let [page, setPage] = useState(1);
   let [total, setTotal] = useState(1);
   let [totalSlices, setTotalSlices] = useState(1);
-  let [slices, setSlices] = useState([]);
+  let [bigSlices, setBigSlices] = useState([]);
   let [smallSlices, setSmallSlices] = useState([]);
   let [projects, setProjects] = useState(null);
 
@@ -25,6 +26,18 @@ function ProjectsSection() {
   useEffect(() => {
     getProjects(1);
   }, []);
+
+  const getSlices = (total, group, array) => {
+    let slices = [Math.ceil(total / group)];
+    for (let i = 0; i < total; i += group) {
+      if (i + group - 1 < total) {
+        slices[i] = array.slice(i, i + group);
+      } else {
+        slices[i] = array.slice(i, total);
+      }
+    }
+    return slices;
+  };
 
   const getProjects = async () => {
     fetch(projectsUrl + '?limit=' + limit, {
@@ -38,30 +51,15 @@ function ProjectsSection() {
         (result) => {
           setPage(1);
 
-          let slices = [Math.ceil(result.totalDocs / group)];
-          for (let i = 0; i < result.totalDocs; i += group) {
-            if (i + group - 1 < result.totalDocs) {
-              slices[i] = result.docs.slice(i, i + group);
-            } else {
-              slices[i] = result.docs.slice(i, result.totalDocs);
-            }
-          }
-          setSlices(slices);
-
-          let smallSlices = [Math.ceil(result.totalDocs / smallGroup)];
-          for (let i = 0; i < result.totalDocs; i += smallGroup) {
-            if (i + smallGroup - 1 < result.totalDocs) {
-              smallSlices[i] = result.docs.slice(i, i + smallGroup);
-            } else {
-              smallSlices[i] = result.docs.slice(i, result.totalDocs);
-            }
-          }
+          bigSlices = getSlices(result.totalDocs, group, result.docs);
+          smallSlices = getSlices(result.totalDocs, smallGroup, result.docs);
+          setBigSlices(bigSlices);
           setSmallSlices(smallSlices);
 
           setTotal(result.totalDocs);
-          if (window.innerWidth > 800) {
+          if (window.innerWidth >= small) {
             setBig(true);
-            setProjects(slices[0]);
+            setProjects(bigSlices[0]);
             setTotalSlices(Math.ceil(result.totalDocs / group));
           } else {
             setBig(false);
@@ -74,8 +72,9 @@ function ProjectsSection() {
         },
       );
   };
+
   function handleResize() {
-    if (window.innerWidth < 800) {
+    if (window.innerWidth < small) {
       if (isBig) {
         setBig(false);
         setProjects(smallSlices[0]);
@@ -84,7 +83,7 @@ function ProjectsSection() {
       }
     } else if (!isBig) {
       setBig(true);
-      setProjects(slices[0]);
+      setProjects(bigSlices[0]);
       setTotalSlices(Math.ceil(total / group));
       setPage(1);
     }
@@ -94,21 +93,15 @@ function ProjectsSection() {
   //Pagination
   const handleChange = (event, value) => {
     setPage(value);
-    if (isBig) {
-      setProjects(slices[value - 1]);
-    } else {
-      setProjects(smallSlices[value - 1]);
-    }
+    let slicesArray = isBig ? bigSlices : smallSlices;
+    setProjects(slicesArray[value - 1]);
   };
 
   const handleArrow = (num) => {
     if ((page < totalSlices && num === 1) || (page > 0 && num === -1)) {
       setPage(page + num);
-      if (isBig) {
-        setProjects(slices[page + num - 1]);
-      } else {
-        setProjects(smallSlices[page + num - 1]);
-      }
+      let slicesArray = isBig ? bigSlices : smallSlices;
+      setProjects(slicesArray[page + num - 1]);
     }
   };
 
@@ -118,12 +111,12 @@ function ProjectsSection() {
       id="Projects"
     >
       <h1 className="title has-text-centered has-text-dark">Trendy projects</h1>
-      <div className="projects__cards is-flex is-justify-content-space-between is-align-items-center">
+      <div className="projects__cards is-flex is-justify-content-space-between is-align-items-center my-2">
         <SvgPrev
           className={page === 1 ? 'arrow-blocked' : 'arrow'}
           onClick={() => handleArrow(-1)}
         />
-        <div className="is-flex is-justify-content-center is-flex-wrap-wrap	mt-2 mb-6">
+        <div className="is-flex is-justify-content-center is-flex-wrap-wrap">
           {projects ? (
             projects.map((item) => <ProjectCard key={item._id} {...item} />)
           ) : (
@@ -135,13 +128,9 @@ function ProjectsSection() {
           onClick={() => handleArrow(1)}
         />
       </div>
-      {slices || smallSlices ? (
-        <div className="projects__pagination">
-          <Pagination count={totalSlices} page={page} onChange={handleChange} />
-        </div>
-      ) : (
-        <div>Loading...</div>
-      )}
+      <div className="projects__pagination">
+        <Pagination count={totalSlices} page={page} onChange={handleChange} />
+      </div>
     </section>
   );
 }
