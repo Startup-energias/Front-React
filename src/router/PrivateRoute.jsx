57cd020d0936c19b330d3317df460ac5843e75c2
chrisdev12 <christianm.bravop@gmail.com>
2021-03-01 01:10:56 -0500

    Solved bug : component should be literraly translate to UpperCase to avoid problems and updated mapStateToProps peroperty

--- src/router/PrivateRoute.jsx
+++ src/router/PrivateRoute.jsx
@@ -2,12 +2,13 @@ import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 import NonAutScreen from '../components/shared/NonAuth';
 
-function PrivateRoute({ auth, Component, ...rest }) {
+// The component should be rendered with upperCase to avoid bugs
+function PrivateRoute({ auth, component: Component, ...rest }) {
   return <Route {...rest} render={() => (auth ? <Component /> : <NonAutScreen />)} />;
 }
 
-const props = (state) => ({
+const mapStateToProps = (state) => ({
   auth: state.user.auth,
 });
 
-export default connect(props, null)(PrivateRoute);
+export default connect(mapStateToProps, null)(PrivateRoute);
